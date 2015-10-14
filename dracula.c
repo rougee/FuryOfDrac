a0e7960ae6557f21841141d97ac7aee2b62c3a87
alvinwp <alvin.w.prijatna@gmail.com>
2015-10-14 15:30:19 +1100

    stuff

--- dracula.c
+++ dracula.c
@@ -14,96 +14,103 @@
 #define TRUE  1
 #define FALSE 0
 
+typedef moveState *_moveState
+
+struct _moveState {
+   LocationID  move;   // The move
+   int         weight; // It's weighting
+};
+
 void decideDraculaMove(DracView gameState)
 {
-	
-	// Get random number
-	srand(time(NULL));
-	int r = rand();
+    
+    // Get random number
+    srand(time(NULL));
+    int r = rand();
 
-	// Create the move character (make default GE)
-	char *move = "GE";
+    // Create the move character (make default GE)
+    char *move = "GE";
 
-	// Get the possible moves
-	int numberOfPossibleMoves;
-	LocationID *possibleMoves = (LocationID*)whereCanIgo(gameState, &numberOfPossibleMoves, 1, 1);
+    // Get the possible moves
+    int numberOfPossibleMoves;
+    LocationID *possibleMoves = (LocationID*)whereCanIgo(gameState, &numberOfPossibleMoves, 1, 1);
 
-	// Get Dracula's trail
-	LocationID trail[TRAIL_SIZE];
-	giveMeTheTrail(gameState, PLAYER_DRACULA, trail);
+    // Get Dracula's trail
+    LocationID trail[TRAIL_SIZE];
+    giveMeTheTrail(gameState, PLAYER_DRACULA, trail);
 
-	int i, j;
+    int i, j;
 
-	if (numberOfPossibleMoves == 0 && giveMeTheRound(gameState) == 0) {
+    if (numberOfPossibleMoves == 0 && giveMeTheRound(gameState) == 0) {
 
-		// Leave the move as the default location (start of game)
+        // Leave the move as the default location (start of game)
 
-	} else if (numberOfPossibleMoves == 0) {
-		// If there are no valid moves, teleport
-		move[0] = 'T';
-		move[1] = 'P';
-	} else {
+    } else if (numberOfPossibleMoves == 0) {
+        // If there are no valid moves, teleport
+        move[0] = 'T';
+        move[1] = 'P';
+    } else {
 
-		// Otherwise, move randomly
-		int canMove = FALSE;
-		int inTrail;
+        // Otherwise, move randomly
+        int canMove = FALSE;
+        int inTrail;
 
-		LocationID *movesWithoutTrail = calloc(NUM_MAP_LOCATIONS, sizeof(LocationID));
-		int movesWithoutTrailNum = 0;
+        LocationID *movesWithoutTrail = calloc(NUM_MAP_LOCATIONS, sizeof(LocationID));
+        int movesWithoutTrailNum = 0;
 
-		LocationID *movesWithoutTrailSea = calloc(NUM_MAP_LOCATIONS, sizeof(LocationID));
-		int movesWithoutTrailSeaNum = 0;
+        LocationID *movesWithoutTrailSea = calloc(NUM_MAP_LOCATIONS, sizeof(LocationID));
+        int movesWithoutTrailSeaNum = 0;
 
-		// Start at 0, and keep going until the move isn't in Dracula's trail
-		LocationID currLocation;
+        // Start at 0, and keep going until the move isn't in Dracula's trail
+        LocationID currLocation;
 
-		// For each possible move, if it's not in the trail, set the move as that
-		for (i=0;i<numberOfPossibleMoves;i++) {
+        // For each possible move, if it's not in the trail, set the move as that
+        for (i=0;i<numberOfPossibleMoves;i++) {
 
-			inTrail = FALSE;
-			currLocation = possibleMoves[i];
-
-
-			for (j=0;j<TRAIL_SIZE;j++) {
-				if (currLocation == trail[j]) {
-					inTrail = TRUE;
-				}
-			}
-
-			
-			// Set the move if not in the trail and break
-			if (!inTrail) {
-
-				// Add it to list of possible moves
-				movesWithoutTrail[movesWithoutTrailNum] = currLocation;
-				movesWithoutTrailNum++;
-
-				// If its not a sea, add it to possible moves with exclude seas
-				if (idToType(currLocation) != SEA) {
-					movesWithoutTrailSea[movesWithoutTrailSeaNum] = currLocation;
-					movesWithoutTrailSeaNum++;
-				}
-
-				canMove = TRUE;
-			}
-
-		}
+            inTrail = FALSE;
+            currLocation = possibleMoves[i];
 
 
-		if (!canMove) {
-			// No valid mvoes, so teleport
-			move = "TP";
-		} else {
+            for (j=0;j<TRAIL_SIZE;j++) {
+                if (currLocation == trail[j]) {
+                    inTrail = TRUE;
+                }
+            }
 
-			// Try for land move, otherwise move into the sea
-			if (movesWithoutTrailNum > 0) {
-				move = idToAbbrev(movesWithoutTrail[r%movesWithoutTrailNum]);
-			} else {
-				move = idToAbbrev(movesWithoutTrailSea[r%movesWithoutTrailSeaNum]);
-			}
+            
+            // Set the move if not in the trail and break
+            if (!inTrail) {
 
-		}
-	}
-	
-	registerBestPlay(move,"");
+                // Add it to list of possible moves
+                movesWithoutTrail[movesWithoutTrailNum] = currLocation;
+                movesWithoutTrailNum++;
+
+                // If its not a sea, add it to possible moves with exclude seas
+                if (idToType(currLocation) != SEA) {
+                    movesWithoutTrailSea[movesWithoutTrailSeaNum] = currLocation;
+                    movesWithoutTrailSeaNum++;
+                }
+
+                canMove = TRUE;
+            }
+
+        }
+
+
+        if (!canMove) {
+            // No valid mvoes, so teleport
+            move = "TP";
+        } else {
+
+            // Try for land move, otherwise move into the sea
+            if (movesWithoutTrailNum > 0) {
+                move = idToAbbrev(movesWithoutTrail[r%movesWithoutTrailNum]);
+            } else {
+                move = idToAbbrev(movesWithoutTrailSea[r%movesWithoutTrailSeaNum]);
+            }
+
+        }
+    }
+    
+    registerBestPlay(move,"");
 }
